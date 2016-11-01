2dc0f84e79b2f3ad00393530c91240e0e2f2bdf4
digachandra <imdigachandra@gmail.com>
2016-11-01 13:37:40 +0700

    Add authenticate token helper

---
+++ server/helpers/authenticate.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
+
+module.exports = {
+  token:verifyToken
+}
+
+function verifyToken(req, res, next){
+  next()
+  var token = req.body.token || req.param('token') || req.headers['x-access-token']
+
+	if(token){
+		jwt.verify(token, req.app.get('secretToken'), function(err, decoded) {
+			if (err) {
+				return res.status(403).json({
+					success: false,
+					message: 'Failed to authenticate token.'
+				})
+			} else {
+				req.decoded = decoded
+				return res.status(200).json({
+					success: true,
+					message: 'Success to authenticate token.'
+				})
+			}
+		});
+	} else {
+    return res.status(403).json({
+      success: false,
+      message: 'No token provided.'
+    })
+	}
+}

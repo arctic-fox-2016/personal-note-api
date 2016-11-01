44ca2570ff1e80b5028e9ef52e46b4801e4d7cd2
digachandra <imdigachandra@gmail.com>
2016-11-01 13:45:23 +0700

    Delete verify token function for create user API

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,7 @@ const authenHelper = require('../helpers/authenticate')
 
 router.get('/users', users.findAll)
 router.get('/users/:id', users.findOne)
-router.post('/users', authenHelper.token, users.createOne)
+router.post('/users', users.createOne)
 router.put('/users/:id', authenHelper.token, users.updateOne)
 router.delete('/users/:id', authenHelper.token, users.deleteOne)
 

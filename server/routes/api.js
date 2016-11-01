c87f504c148e54468fb402974bacf74bb79a992d
digachandra <imdigachandra@gmail.com>
2016-11-01 16:15:28 +0700

    Update API routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const users = require('../controllers/users')
+const notes = require('../controllers/notes')
 const authenHelper = require('../helpers/authenticate')
 
 router.get('/users', users.findAll)
@@ -8,5 +9,11 @@ router.get('/users/:id', users.findOne)
 router.post('/users', users.createOne)
 router.put('/users/:id', authenHelper.token, users.updateOne)
 router.delete('/users/:id', authenHelper.token, users.deleteOne)
+
+router.get('/notes', authenHelper.token, notes.findAll)
+router.get('/notes/:id', authenHelper.token, notes.findOne)
+router.post('/notes', authenHelper.token, notes.createOne)
+router.put('/notes/:id', authenHelper.token, notes.updateOne)
+router.delete('/notes/:id', authenHelper.token, notes.deleteOne)
 
 module.exports = router

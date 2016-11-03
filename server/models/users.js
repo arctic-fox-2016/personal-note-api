8721562e636f86a399cdffe08233fe415d52d16b
ivangerard <ivan.gerard@hotmail.com>
2016-11-03 16:32:11 +0700

    updated model

--- server/models/users.js
+++ server/models/users.js
@@ -7,6 +7,9 @@ var Schema = mongoose.Schema;
 var userSchema = mongoose.Schema({
     name: String,
     age: Number,
+    title:String,
+    content:String,
+    id:String,
     notes: [{
         title: String,
         content:String,
